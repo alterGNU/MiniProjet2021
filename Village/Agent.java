809f1f16bfdb6b3c88c9ab8cd3a500a8aa02c85c
alterGNU <alterGNU@gmail.com>
2020-11-19 02:01:58 +0100

    CLEAN-UP:Réorganisation dans dossier Village & CREATION class Agent

---
+++ Village/Agent.java
@@ -0,0 +1,11 @@
+/**
+ * TODO 
+ * - Classe Agent () : Définis par sa position sur le terrain
+ * - Variables/Attribu
+ *     - [ ] : coordonnée (int,int) : correspond à sa position sur le terrain
+ * - Méthodes
+ *     - [ ] : distance(x,y) : retourne la distance séparant l'agent de la ressource
+ *     - [ ] : seDeplacer(xnew,ynew) : change la position de l'objet se déplaçant sur le terrain
+ */
+public class Agent {
+}
