108b75d3f3d0032aad345b519591e2c5383fa882
alterGNU <alterGNU@gmail.com>
2020-11-20 04:59:31 +0100

    ADD-ListRessource à simulation, sur laquelle looper pour init

--- Village/Simulation.java
+++ Village/Simulation.java
@@ -1,15 +1,22 @@
 import java.util.Random;
 
+/** Classe agent 
+ *
+ * @param ter1 : Correspond au terrain que contient la simulation
+ * @param tabAgent : Tableau contenant les agents présent dans la simulation
+ * @param tabRessource : Tableau contenant le nom et le nombre de ressources à ajouter au terrain
+ */
 public class Simulation {
 
     public static Terrain ter1 = new Terrain(10,10);
+    public static String[][] tabRessource = {{"Roche","3"},{"Arbre","5"}};
 
     public static void phaseInit(){
         System.out.println("PHASE 1:Initialisation de l'environnement:");
-        // Faire loop sur liste ensuite
+        
         // Initialisation : Créer l'environnement
-        initRandomRessources("Roche", 5) ;
-        initRandomRessources("Arbre", 5) ;
+        initRandomRessources(tabRessource) ;
+        
         // Affichage du terrain avec les ressources ajoutées
         System.out.println("Informations sur le terrain:\n"+ter1);
         ter1.affiche();
@@ -20,21 +27,25 @@ public class Simulation {
      * @param nom Correspond au nom du type de ressource à créer (exemple : Roche, Arbre,...)
      * @param m Correspond au nombre de ressource à créer
      */
-    public static void initRandomRessources(String nom, int m){// puis int n pour les agents
-        int compteobjet = 0; // Compteur du nombre de ressource/objet créé(e)s
-        while (compteobjet < m) {
-            // Génération aléatoire des coordonnées et de la quantité des Arbres à créer
-            int x = new Random().nextInt(ter1.nbLignes); // Valeur aléatoire d'abscisse comprise entre 1 et m
-            int y = new Random().nextInt(ter1.nbColonnes); // Valeur aléatoire d'ordonnée comprise entre 1 et m
-            int q = new Random().nextInt(3)+1; // Valeur aléatoire de quantité comprise entre 1 et nb
+    public static void initRandomRessources(String[][] tab){// puis int n pour les agents
+        for (int i =0;i<tab.length;i++){
+            String nom = tab[i][0];
+            int m = Integer.parseInt(tab[i][1]);
+            int compteobjet = 0; // Compteur du nombre de ressource/objet créé(e)s
+            while (compteobjet < m) {
+                // Génération aléatoire des coordonnées et de la quantité des Arbres à créer
+                int x = new Random().nextInt(ter1.nbLignes); // Valeur aléatoire d'abscisse comprise entre 1 et m
+                int y = new Random().nextInt(ter1.nbColonnes); // Valeur aléatoire d'ordonnée comprise entre 1 et m
+                int q = new Random().nextInt(3)+1; // Valeur aléatoire de quantité comprise entre 1 et nb
 
-            // Vérification si Vide, alors 
-            if (ter1.caseEstVide(x,y)){
-                Ressource res1 = new Ressource(nom,m);
-                ter1.setCase(x,y,res1);
-                compteobjet++ ;
+                // Vérification si Vide, alors 
+                if (ter1.caseEstVide(x,y)){
+                    Ressource res1 = new Ressource(nom,m);
+                    ter1.setCase(x,y,res1);
+                    compteobjet++ ;
+                }
             }
+            System.out.println("..."+m+" ressources "+nom+" ont été ajoutées...");
         }
-        System.out.println("..."+m+" ressources "+nom+" ont été ajoutées...");
     }
 }
--- Village/TestSimulation.java
+++ Village/TestSimulation.java
@@ -7,6 +7,8 @@ public class TestSimulation {
 	 * @param args
 	 */
     public static void main(String[] args) {
+        //public static Terrain ter1 = new Terrain(10,10);
+        //private static String[][] tabRessource = {{"Roche","3"},{"Arbre","5"}};
         Simulation.phaseInit();
     }
 
